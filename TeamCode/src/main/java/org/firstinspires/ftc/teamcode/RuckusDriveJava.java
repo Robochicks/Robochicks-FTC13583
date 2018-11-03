dfe2d5510ea6864a15997776c7668091c4c3c55a
robochicks <robochicks13583@gmail.com>
2018-11-03 10:53:50 -0400

    We added the new motor for the arm that collects the elements.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RuckusDriveJava.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RuckusDriveJava.java
@@ -25,6 +25,7 @@ public class RuckusDriveJava extends LinearOpMode {
     private Blinker expansion_Hub_2;
     private Blinker expansion_Hub_3;
     private ElapsedTime runtime = new ElapsedTime();
+    private DcMotor spin;
 
     @Override
     public void runOpMode() {
@@ -39,6 +40,7 @@ public class RuckusDriveJava extends LinearOpMode {
         bl = hardwareMap.get(DcMotor.class, "BL");
         br = hardwareMap.get(DcMotor.class, "BR");
         arm = hardwareMap.get(DcMotor.class, "Arm");
+        spin = hardwareMap.get(DcMotor.class, "spin");
 
         // Most robots need the motor on one side to be reversed to drive forward
         // Reverse the motor that runs backwards when connected directly to the battery
@@ -47,6 +49,7 @@ public class RuckusDriveJava extends LinearOpMode {
         bl.setDirection(DcMotor.Direction.REVERSE);
         br.setDirection(DcMotor.Direction.FORWARD);
         arm.setDirection(DcMotor.Direction.FORWARD);
+        spin.setDirection(DcMotor.Direction.FORWARD);
 
         // Wait for the game to start (driver presses PLAY)
         waitForStart();
@@ -61,6 +64,7 @@ public class RuckusDriveJava extends LinearOpMode {
             double blPower;
             double brPower;
             double armPower;
+            double spinPower;
 
             // Choose to drive using either Tank Mode, or POV Mode
             // Comment out the method that's not used.  The default below is POV.
@@ -71,18 +75,20 @@ public class RuckusDriveJava extends LinearOpMode {
             double x1 = gamepad1.left_stick_x;
             double x2 = gamepad1.right_stick_x;
             double y1Operator = gamepad2.left_stick_y;
+            double y2Operator = gamepad2.right_stick_y;
             flPower   = Range.clip(y1 + x1 + x2, -1.0, 1.0) ;
             frPower   = Range.clip(y1 - x1 + x2, -1.0, 1.0) ;
             blPower   = Range.clip(y1 - x1 - x2, -1.0, 1.0) ;
             brPower   = Range.clip(y1 + x1 - x2, -1.0, 1.0) ;
             armPower   = Range.clip( y1Operator, -1.0, 1.0);
+            spinPower = Range.clip(y2Operator, -1.0, 1.0);
 
 
             // Tank Mode uses one stick to control each wheel.
             // - This requires no math, but it is hard to drive forward slowly and keep straight.
             // leftPower  = -gamepad1.left_stick_y ;
             // rightPower = -gamepad1.right_stick_y ;
-            SetDrivePower(flPower, frPower, blPower, brPower, armPower);
+            SetDrivePower(flPower, frPower, blPower, brPower, armPower, spinPower);
             // Send calculated power to wheels
 
 
@@ -95,12 +101,13 @@ public class RuckusDriveJava extends LinearOpMode {
         }
     }
 
-    private void SetDrivePower(double FrontLeftPower, double FrontRightPower, double BackLeftPower, double BackRightPower, double ArmPower){
+    private void SetDrivePower(double FrontLeftPower, double FrontRightPower, double BackLeftPower, double BackRightPower, double ArmPower, double SpinPower){
         fl.setPower(FrontLeftPower);
         fr.setPower(FrontRightPower);
         bl.setPower(BackLeftPower);
         br.setPower(BackRightPower);
         arm.setPower(ArmPower);
+        spin.setPower(SpinPower);
 
     }
 
