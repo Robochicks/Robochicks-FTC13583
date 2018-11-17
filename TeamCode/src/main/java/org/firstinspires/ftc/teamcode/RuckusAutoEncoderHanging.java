2b35b55baf9c5f591e787533a66f56d63e0c8051
robochicks <robochicks13583@gmail.com>
2018-11-16 21:16:38 -0500

    Mia Karl 11/16/18 Hanging auto

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RuckusAutoEncoderHanging.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RuckusAutoEncoderHanging.java
@@ -2,19 +2,16 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.Blinker;
 import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Gyroscope;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
-import com.qualcomm.robotcore.util.Range;
 //import com.qualcomm.robotcore.external.Telemetry;
 
-@Autonomous(name="RuckusAutoEncoder")
+@Autonomous(name="Ruckus Auto Encoder Hanging")
 
-public class RuckusAutoEncoder extends LinearOpMode {
+public class RuckusAutoEncoderHanging extends LinearOpMode {
     private Gyroscope imu;
     private Gyroscope imu_1;
     private DcMotor fl;
@@ -36,67 +33,54 @@ public class RuckusAutoEncoder extends LinearOpMode {
 
 
         // Send telemetry message to signify robot waiting;
-        telemetry.addData("Status", "Ready to run");    //
+        telemetry.addData("Status", "Ready to run");
         telemetry.update();
+
 
         // Initialize the hardware variables. Note that the strings used here as parameters
         // to 'get' must correspond to the names assigned during the robot configuration
         // step (using the FTC Robot Controller app on the phone).
-        fl  = hardwareMap.get(DcMotor.class, "FL");
+        fl = hardwareMap.get(DcMotor.class, "FL");
         fr = hardwareMap.get(DcMotor.class, "FR");
         bl = hardwareMap.get(DcMotor.class, "BL");
         br = hardwareMap.get(DcMotor.class, "BR");
         arm = hardwareMap.get(DcMotor.class, "Arm");
         spin = hardwareMap.get(DcMotor.class, "Spin");
-        hook = hardwareMap. get(Servo.class, "hook");
+        hook = hardwareMap.get(Servo.class, "hook");
 
-        // Most robots need the motor on one side to be reversed to drive forward
-        // Reverse the motor that runs backwards when connected directly to the battery
-        fl.setDirection(DcMotor.Direction.REVERSE);
-        fr.setDirection(DcMotor.Direction.FORWARD);
-        bl.setDirection(DcMotor.Direction.REVERSE);
-        br.setDirection(DcMotor.Direction.FORWARD);
-        arm.setDirection(DcMotor.Direction.FORWARD);
-        spin.setDirection(DcMotor.Direction.FORWARD);
+        telemetry.addData("Movement", "Lowering");
 
-        telemetry.addData("EncoderMovement", "Waiting");
-        telemetry.update();
-
-        waitForStart();
-
-        telemetry.addData("EncoderMovement", "Driving Forward");
-        telemetry.update();
-
-        //Lowering the robot to the floor
         arm.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         arm.setTargetPosition(1000);
         arm.setMode(DcMotor.RunMode.RUN_TO_POSITION);
         arm.setPower(0.2);
 
-        sleep(1000);
+        while (arm.isBusy()) {
+            telemetry.addData("Mode", "Moving");
+            telemetry.addData("Distance ARM", arm.getCurrentPosition());
+            telemetry.addData("Busy ARM", arm.isBusy());
+            telemetry.update();
+        }
 
-        SetDriveDistance(4020, 4020, 4020, 4020, 0.3, 0.3, 0.3, 0.3);
+        hook.setPosition(0);
 
+        arm.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        arm.setTargetPosition(-1000);
+        arm.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        arm.setPower(0.2);
 
-        telemetry.addData("EncoderMovement", "Turning");
-        telemetry.update();
-
-        sleep(1000);
-
-        spin.setPower(-0.2);
-        sleep(1000);
-        spin.setPower(0);
-
-        SetDriveDistance(2000, -2000, 2000, -2000, 0.3, 0.3, 0.3, 0.3);
-        sleep(3000);
-
-        SetDriveDistance(8075, 8075, 8075, 8075, 0.1, 0.1, 0.1, 0.1);
-        sleep(5000);
+        while (arm.isBusy()) {
+            telemetry.addData("Mode", "Moving");
+            telemetry.addData("Distance ARM", arm.getCurrentPosition());
+            telemetry.addData("Busy ARM", arm.isBusy());
+            telemetry.update();
+        }
 
         telemetry.addData("EncoderMovement", "Complete");
         telemetry.update();
     }
-    private void SetDriveDistance(int FrontLeftDistance, int FrontRightDistance, int BackLeftDistance, int BackRightDistance, double FrontLeftPower, double FrontRightPower, double BackLeftPower, double BackRightPower){
+
+    private void SetDriveDistance(int FrontLeftDistance, int FrontRightDistance, int BackLeftDistance, int BackRightDistance, double FrontLeftPower, double FrontRightPower, double BackLeftPower, double BackRightPower) {
         bl.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         br.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         fl.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
@@ -117,17 +101,17 @@ public class RuckusAutoEncoder extends LinearOpMode {
         fl.setPower(FrontLeftPower);
         fr.setPower(FrontRightPower);
 
-        while(fl.isBusy() || bl.isBusy() || fr.isBusy() || br.isBusy()) {
-            telemetry.addData("Mode" , "Moving");
-            telemetry.addData( "Distance BL", bl.getCurrentPosition());
-            telemetry.addData( "Distance BR", br.getCurrentPosition());
-            telemetry.addData( "Distance FL", fl.getCurrentPosition());
-            telemetry.addData( "Distance FR", fr.getCurrentPosition());
+        while (fl.isBusy() || bl.isBusy() || fr.isBusy() || br.isBusy()) {
+            telemetry.addData("Mode", "Moving");
+            telemetry.addData("Distance BL", bl.getCurrentPosition());
+            telemetry.addData("Distance BR", br.getCurrentPosition());
+            telemetry.addData("Distance FL", fl.getCurrentPosition());
+            telemetry.addData("Distance FR", fr.getCurrentPosition());
 
-            telemetry.addData( "Busy BL", bl.isBusy());
-            telemetry.addData( "Busy BR", br.isBusy());
-            telemetry.addData( "Busy FL", fl.isBusy());
-            telemetry.addData( "Busy FR", fr.isBusy());
+            telemetry.addData("Busy BL", bl.isBusy());
+            telemetry.addData("Busy BR", br.isBusy());
+            telemetry.addData("Busy FL", fl.isBusy());
+            telemetry.addData("Busy FR", fr.isBusy());
 
            /* telemetry.addData( "Distance",  "Front Left (%.2f), Front Right (%.2f), " +
                     "Back Left (%.2f), Back Right (%.2f)",
@@ -136,16 +120,16 @@ public class RuckusAutoEncoder extends LinearOpMode {
                             "Back Left (%.2f), Back Right (%.2f)",
                     String.valueOf(fl.isBusy()),  String.valueOf(fr.isBusy()),  String.valueOf(bl.isBusy()), String.valueOf(br.isBusy()));*/
             telemetry.update();
+
+            bl.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+            br.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+            fl.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+            fr.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+            telemetry.addData("EncoderMovement", "Complete");
+            telemetry.update();
+
+
         }
-
-        bl.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        br.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        fl.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        fr.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-
-        telemetry.addData("EncoderMovement", "Complete");
-        telemetry.update();
-
-
     }
 }
